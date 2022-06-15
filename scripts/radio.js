967d86e52672c4cbcb7cc6fd70296ef5491184ea
eliasmenezesjr <eliasmenezes2510@gmail.com>
2022-06-15 18:26:15 -0300

    add body.each in JS

--- scripts/radio.js
+++ scripts/radio.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
-  $('.articleContent').each(function () { 
-    var radioExercicio = $('.wrap-radios');
+  $('.articleContent').each(function () {
+    $('.body').each(function () {
+      var radioExercicio = $('.wrap-radios');
 
-    function radioId() {
-      radioExercicio = $('.wrap-radios');
-      radioExercicio.find('input').ready(function () {
-        radioExercicio.each(function () {
-          var randomId = Math.floor(Math.random() * 1000000) + 1;
-          $(this).attr('id', randomId);
-          $(this).find('input').attr('name', randomId);
+      function radioId() {
+        radioExercicio = $('.wrap-radios');
+        radioExercicio.find('input').ready(function () {
+          radioExercicio.each(function () {
+            var randomId = Math.floor(Math.random() * 1000000) + 1;
+            $(this).attr('id', randomId);
+            $(this).find('input').attr('name', randomId);
+          });
         });
-      });
-    }
-  
-    radioExercicio.find('input').ready(radioId);
-    radioExercicio.find('.textradio').on('click', radioId);
-  })
-})
+      }
+      
+      radioExercicio.find('input').ready(radioId);
+      radioExercicio.find('.textradio').on('click', radioId);
+    });
+  });
+});
